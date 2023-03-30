f4bbf17c6f93cc9b970d4792e410896651913036
Renier Jordan <renier.jordan@hotmail.com>
2023-03-30 15:13:05 +0200

    minor changes

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -3,6 +3,8 @@ const searchInput = document.querySelector('.search-input');
 
 function search(value) {
     
+    if (value.length>2) console.log(value);
+    else console.log("Veuillez entrez au moins 3 caractères")
 }
 
 
