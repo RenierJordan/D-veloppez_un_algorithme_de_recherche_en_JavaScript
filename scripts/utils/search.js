7ac0f4388c457070be8c2237c720847fc27746cd
Renier Jordan <renier.jordan@hotmail.com>
2023-03-30 15:19:11 +0200

    test

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -4,7 +4,7 @@ const searchInput = document.querySelector('.search-input');
 function search(value) {
     
     if (value.length>2) console.log(value);
-    else console.log("Veuillez entrez au moins 3 caractères")
+    else console.log("Veuillez entrez au moins 3 caractères.")
 }
 
 
