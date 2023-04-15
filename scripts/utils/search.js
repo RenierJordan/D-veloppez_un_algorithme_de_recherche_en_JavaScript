ee3ae9435589dc980154773254adc81d6a429d92
Renier Jordan <renier.jordan@hotmail.com>
2023-04-15 16:08:20 +0200

    fonction de tri globale

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,5 +1,6 @@
 import { getRecipes, UpdateRecipes } from "../pages/index.js";
 import { arrayUnique } from "../utils/function.js";
+import { tagsApplied, SortByTag } from "../utils/tags.js";
 
 const searchInput = document.querySelector('.search-input');
 const getrecipes = await getRecipes();
@@ -8,26 +9,15 @@ let LastSorted = getrecipes;
 
 
 
-function SortRecipes(value){
 
-        let sortedbyname = getrecipes.filter(recipes => recipes.name.toLowerCase().includes(value.toLowerCase()))
-        let sortedbydesc = getrecipes.filter(recipes => recipes.description.toLowerCase().includes(value.toLowerCase()))
-        let sortedbying = getrecipes.filter(recipes => recipes.ingredients.find(ingre => ingre.ingredient.toLowerCase().includes(value.toLowerCase())));
-
-        LastSorted = arrayUnique((sortedbyname.concat(sortedbydesc)).concat(sortedbying))
-    
-    
-    return LastSorted;
-}
-
-function search(value) {
+export function search(value) {
     
     if (value.length>2){
-        let sorted = SortRecipes(value);
+        let sorted = FilterGlobal(value,tagsApplied);
         UpdateRecipes(sorted);
     } 
     else if (value.length==0){
-        let sorted = SortRecipes(value);
+        let sorted = FilterGlobal(value,tagsApplied);
         UpdateRecipes(sorted);
     } 
     else console.log("Veuillez entrez au moins 3 caractères.")
@@ -36,5 +26,22 @@ function search(value) {
 export function getLastSorted() {
     return LastSorted;
 }
+export function setLastSorted(value) {
+    LastSorted= value;
+}
 
-searchInput.addEventListener("change", ()=>search(searchInput.value));
+searchInput.addEventListener("change", ()=>search(searchInput.value,tagsApplied));
+
+
+export function FilterGlobal(value,tags){
+    let sortedbyname = getrecipes.filter(recipes => recipes.name.toLowerCase().includes(value.toLowerCase()))
+    let sortedbydesc = getrecipes.filter(recipes => recipes.description.toLowerCase().includes(value.toLowerCase()))
+    let sortedbying = getrecipes.filter(recipes => recipes.ingredients.find(ingre => ingre.ingredient.toLowerCase().includes(value.toLowerCase())));    
+    let Sorted = arrayUnique((sortedbyname.concat(sortedbydesc)).concat(sortedbying))
+
+    tags.forEach(tag => {
+        Sorted = SortByTag(Sorted,tag)
+    });
+    setLastSorted(Sorted)
+    return Sorted;
+}
--- scripts/utils/tags.js
+++ scripts/utils/tags.js
@@ -1,128 +1,183 @@
-import { UpdateRecipes } from "../pages/index.js";
-import { getLastSorted } from "../utils/search.js";
+import { getRecipes, UpdateRecipes } from "../pages/index.js";
+import { getLastSorted, FilterGlobal } from "../utils/search.js";
 import { arrayUnique } from "../utils/function.js";
 
-function GetTags(data, Options){
-    let tags = [];
-    let ID = Options.getAttribute("id")
-    tags.push(ID)
-    if ( ID=="ingredients"){
-        data.forEach((recipes) => {
-                const recipesModel = recipesFactory(recipes);
-                const ingredients = recipesModel.ingredients;
-                ingredients.forEach((ingredient) => {
-                    tags.push(ingredient.ingredient)
-                });
-            });
-    }
-    else if (ID=="appliance"){
-        data.forEach((recipes) => {
-                const recipesModel = recipesFactory(recipes);
-                const appareils = recipesModel.appliance;
-                tags.push(appareils)              
-            });
-    }
-    else {
-        data.forEach((recipes) => {
-            const recipesModel = recipesFactory(recipes);
-            const ustensils = recipesModel.ustensils;
-            for (let i=0; i<ustensils.length; i++ ){
-                tags.push(ustensils[i])
-            }      
-        });
-    }
-    
-    tags = arrayUnique(tags)
-    console.log(tags)
-    return tags;
+const searchInput = document.querySelector(".search-input");
+let tagsApplied = [];
+
+export {tagsApplied}
+
+function GetTags(data, Options) {
+  let tags = [];
+  let ID = Options.getAttribute("id");
+  tags.push(ID);
+  if (ID == "ingredients") {
+    data.forEach((recipes) => {
+      const recipesModel = recipesFactory(recipes);
+      const ingredients = recipesModel.ingredients;
+      ingredients.forEach((ingredient) => {
+        tags.push(ingredient.ingredient);
+      });
+    });
+  } else if (ID == "appliance") {
+    data.forEach((recipes) => {
+      const recipesModel = recipesFactory(recipes);
+      const appareils = recipesModel.appliance;
+      tags.push(appareils);
+    });
+  } else {
+    data.forEach((recipes) => {
+      const recipesModel = recipesFactory(recipes);
+      const ustensils = recipesModel.ustensils;
+      ustensils.forEach((ustensil) => {
+        tags.push(ustensil);
+      });
+    });
+  }
+
+  tags = arrayUnique(tags);
+  console.log(tags);
+  return tags;
 }
 
-function expand(tags){
-    tags.setAttribute("data-set","expand");
-    tags.firstElementChild.lastElementChild.setAttribute("src","./assets/expand_more-24px 5.svg")
+function expand(tags) {
+  tags.setAttribute("data-set", "expand");
+  tags.firstElementChild.lastElementChild.setAttribute(
+    "src",
+    "./assets/expand_more-24px 5.svg"
+  );
 
-    let LastSorted= getLastSorted();
-    let tagsList = GetTags(LastSorted, tags);
+  let LastSorted = getLastSorted();
+  let tagsList = GetTags(LastSorted, tags);
 
-    let tagsDiv = document.createElement("div")
-    tagsDiv.setAttribute("class","tagsDiv")
-    
+  let tagsDiv = document.createElement("div");
+  tagsDiv.setAttribute("class", "tagsDiv");
 
-    const ingredientsList = document.createElement( 'ul' );
-        for(let i =1; i<tagsList.length; i++){
-             let li = document.createElement('li');
-            li.innerHTML = tagsList[i];
-            li.setAttribute("data-set",tagsList[0])
-            li.addEventListener("click", ()=>ApplyTags(tagsList[i],tagsList[0]))         
-            ingredientsList.appendChild(li);
-        }
-    tagsDiv.appendChild(ingredientsList)
-    tags.appendChild(tagsDiv);
-
+  const ingredientsList = document.createElement("ul");
+  for (let i = 1; i < tagsList.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = tagsList[i];
+    li.setAttribute("data-set", tagsList[0]);
+    li.addEventListener("click", () => ApplyTag(tagsList[i], tagsList[0]));
+    ingredientsList.appendChild(li);
+  }
+  tagsDiv.appendChild(ingredientsList);
+  tags.appendChild(tagsDiv);
 }
 
-function retract(tags){
-    tags.removeAttribute("data-set","expand");
-    tags.firstElementChild.lastElementChild.setAttribute("src","./assets/expand_more-24px 4.svg")
+function retract(tags) {
+  tags.removeAttribute("data-set", "expand");
+  tags.firstElementChild.lastElementChild.setAttribute(
+    "src",
+    "./assets/expand_more-24px 4.svg"
+  );
 
-    let tagsDiv = document.querySelector(".tagsDiv")
-    tagsDiv.remove()
+  tags.lastElementChild.remove();
 }
 
-function Modify(tags){
-    if(tags.hasAttribute("data-set","expand")){
+function Modify(tags) {
+  if (tags.hasAttribute("data-set", "expand")) {
     retract(tags);
-    }
-    else expand(tags);
+  } else expand(tags);
 }
 
-function ApplyTags(tag,tagType) {
-    const Showtag = document.querySelector(".Showtag")
-    Showtag.setAttribute("data-set",tagType)
-    Showtag.style.display= "flex"
-    Showtag.firstChild.textContent = tag;
+function ApplyTag(tag, tagType) {
+  const Showtag = document.querySelector(".Showtag");
+  Showtag.style.display = "flex";
 
-    
-    let Sorted = SortByTag(tag,tagType)
-    UpdateRecipes(Sorted)
+  const tagbtn = document.createElement("div");
+  tagbtn.setAttribute("data-set", tagType);
+  tagbtn.textContent = tag;
+
+  const Close = document.createElement("i");
+  Close.setAttribute("class", "fa-regular fa-circle-xmark svg close-img");
+  Close.addEventListener("click", () => CloseTag(tag));
+
+  tagbtn.appendChild(Close);
+  Showtag.appendChild(tagbtn);
+
+  tagsApplied.push(tag);
+  console.log(tagsApplied);
+  let Sorted = FilterGlobal(searchInput.value, tagsApplied);
+  UpdateRecipes(Sorted);
 }
 
-function SortByTag(tag,tagType){
-    let LastSorted = getLastSorted();
-    if (tagType == "ingredients"){
-        let result = LastSorted.filter(recipes => recipes.ingredients.find(ingre => ingre.ingredient.toLowerCase().includes(tag.toLowerCase())));  
-        return(result)     
-    }
-
-    else if (tagType == "appliance"){
-        let result = LastSorted.filter(recipes => recipes.appliance.toLowerCase().includes(tag.toLowerCase()))
-        return(result)
-    }
-
-    else if (tagType == "ustensils"){
-        let result = LastSorted.filter(recipes => recipes.ustensils.find(ustensils => ustensils.toLowerCase().includes(tag.toLowerCase())))
-        return(result)
-    }
-        
-    else return LastSorted
+function RemoveTag(tag) {
+  tagsApplied = tagsApplied.filter((_tag) => tag !== _tag);
+  let Sorted = FilterGlobal(searchInput.value, tagsApplied);
+  UpdateRecipes(Sorted);
+  console.log(tagsApplied);
 }
 
+export function SortByTag(Sorted, tag) {
+  if (
+    Sorted.find((recipes) =>
+      recipes.ingredients.find(
+        (ingre) => ingre.ingredient.toLowerCase() === tag.toLowerCase()
+      )
+    )
+  ) {
+    let result = Sorted.filter((recipes) =>
+      recipes.ingredients.find(
+        (ingre) => ingre.ingredient.toLowerCase() === tag.toLowerCase()
+      )
+    );
+    return result;
+  } else if (
+    Sorted.find(
+      (recipes) => recipes.appliance.toLowerCase() === tag.toLowerCase()
+    )
+  ) {
+    let result = Sorted.filter(
+      (recipes) => recipes.appliance.toLowerCase() === tag.toLowerCase()
+    );
+    return result;
+  } else if (
+    Sorted.find((recipes) =>
+      recipes.ustensils.find(
+        (ustensils) => ustensils.toLowerCase() === tag.toLowerCase()
+      )
+    )
+  ) {
+    let result = Sorted.filter((recipes) =>
+      recipes.ustensils.find(
+        (ustensils) => ustensils.toLowerCase() === tag.toLowerCase()
+      )
+    );
+    return result;
+  } else return Sorted;
+}
 
-function CloseTag(){
-    let LastSorted = getLastSorted();
-    UpdateRecipes(LastSorted)
-
-    const Showtag = document.querySelector(".Showtag")
-    Showtag.removeAttribute("data-set")
-    Showtag.style.display= "none"
-    Showtag.firstChild.textContent = "";
-
+export function CloseTag(tag) {
+  const Showtag = document.querySelector(".Showtag");
+  Showtag.childNodes.forEach((node) => {
+    if (node.textContent === tag) {
+      node.remove();
+    }
+  });
+  RemoveTag(tag);
+  if (!Showtag.hasChildNodes) {
+    Showtag.style.display = "none";
+  }
 }
 
 let tags = document.querySelectorAll(".tags");
-for (let i=0; i<tags.length; i++){  
-    tags[i].addEventListener("click", ()=>Modify(tags[i]));   
-}
+for (let i = 0; i < tags.length; i++) {
+  tags[i].addEventListener("click", () => Modify(tags[i]));
+  tags[i].addEventListener("blur", () => retract(tags[i]));
 
-let close = document.querySelector(".close-img");
-close.addEventListener("click", ()=>CloseTag());
+  //Permet de fermet les dropdown de tags lorsque l'on clique en dehors
+  document.addEventListener("click", (evt) => {
+  let targetEl = evt.target; // clicked element      
+  do {
+    if(targetEl == tags[i]) {
+      return;
+    }
+    // Go up the DOM
+    targetEl = targetEl.parentNode;
+  } while (targetEl);    
+  if (tags[i].hasAttribute("data-set", "expand")) {
+    retract(tags[i]);
+  }
+});
+}
